7e3234268c736bce8f31bcb0856bcf1cf67ea04b
Krithiv-7 <krithivjayaprakash@gmail.com>
2025-08-24 13:27:50 +0530

    package update

--- events/ready.js
+++ events/ready.js
@@ -1,7 +1,7 @@
 const { REST, Routes, ActivityType } = require('discord.js');
 
 module.exports = {
-    name: 'ready',
+    name: 'clientReady',
     once: true,
     async execute(client) {
         console.log(`Ready! Logged in as ${client.user.tag}`);
